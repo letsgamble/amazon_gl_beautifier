50c93bc366629597416ebaafa0036d54812cc0be
Kamil Wesołowski <letsgamble92@gmail.com>
2022-09-19 10:28:32 +0200

    final 1.0 ver

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, request, session
 
 app = Flask(__name__)
 app.secret_key = 'many random bytes'
 
-input_lists = []
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    combined_list = list(dict.fromkeys(input_lists))
-    replaced_list = [lst.replace('gl_', '') for lst in combined_list]
-    _replaced_list = [lst.replace('_', ' ') for lst in replaced_list]
-    return render_template('index.html', content=_replaced_list)
-
-
-@app.route('/add_list/', methods=['GET', 'POST'])
-def add_list():
+    if session.get("list") is None:
+        session["list"] = []
     if request.method == 'POST':
-        lst = request.form.get('gls')
-        if len(list(lst)) > 0:
-            input_lists.extend(list(lst.split(', ')))
-            return redirect(url_for('main'))
+        session['gls'] = request.form.get('gls')
+        session["list"].extend(list(session['gls'].split(', ')))
+        combined_list = list(dict.fromkeys(session["list"]))
+        replaced_list = [lst.replace('gl_', '') for lst in combined_list]
+        _replaced_list = [lst.replace('_', ' ') for lst in replaced_list]
+        return render_template('index.html', content=_replaced_list)
+    return render_template('index.html')
+
+
+@app.route('/delete/')
+def delete():
+    print('sesja usunieta')
+    session["list"] = []
+    session.clear()
+    return redirect(url_for('main'))
